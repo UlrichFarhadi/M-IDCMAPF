6c484b49d5993cf3065a7d31f5df4a042c59164f
UlrichFarhadi <Ulrichfarhadi@hotmail.com>
2023-07-06 20:40:47 +0200

    .

--- GA_Training_Benchmark_Maps/train_scheduler.py
+++ GA_Training_Benchmark_Maps/train_scheduler.py
@@ -1,7 +1,9 @@
 import subprocess
 import os
 
-command = ['python3', "GA_Training_Benchmark_Maps/train_benchmark.py"]
+python_ver = 'python3'
+command = [python_ver, "GA_Training_Benchmark_Maps/train_benchmark.py"]
+command_ttest = [python_ver, "GA_Training_Benchmark_Maps/ttest_benchmark_maps.py"]
 
 # Execute the command and capture the output
 while True:
@@ -13,5 +15,7 @@ while True:
         # Remove the flag file
         os.remove('GA_Training_Benchmark_Maps/inner_script_complete.txt')
         # Exit the loop or continue with further processing
+        print("Running Statistical Test")
+        subprocess.run(command_ttest)
         break
 
